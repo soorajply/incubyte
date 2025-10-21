7f5c5e991b27383fb1b3c19df925d5e457061c5f
SOORAJ <sooraj@healthplotter.com>
2025-10-21 17:44:31 +0530

    STEP 2: When input has multiple numbers it should return the sum of all numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -21,5 +21,11 @@ RSpec.describe StringCalculator do
       end
     end
 
+    context 'when input has multiple numbers' do
+      it 'returns the sum of all numbers' do
+        expect(StringCalculator.add("1,2,3,4,5")).to eq(15)
+      end
+    end
+
   end
 end
